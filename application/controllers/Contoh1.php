231e11fac7c175687d72a591a6792b55136196d8
RianyWulandari <rianywulandr16@gmail.com>
2023-10-10 16:12:25 +0700

    P1 add Contoh1.php

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -3,6 +3,7 @@ class contoh1 extends CI_controller
 {
     public function index()
     {
+        //Contoh1.php//
         echo "<h1>Perkenalkan</h1>";
         echo"Nama Saya Riany Wulandari
             Saya tinggal di Bekasi
