ad2ceaf3c0390ba3c6e18ca90d5616ebe3210db5
RianyWulandari <rianywulandr16@gmail.com>
2023-12-07 16:54:22 +0700

    P12 Header

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -12,7 +12,7 @@
 <body>
 	<nav class="navbar navbar-default">
 		<div class="container">
-			<!-- Brand and toggle get grouped for better mobile display -->
+			<!--  -->
 			<div class="navbar-header">
 				<button type="button" class="navbar-togglecollapsed" data-toggle="collapse" data-target="#bs-example-navbarcollapse-1" aria-expanded="false">
 					<span class="sr-only">Toggle navigation</span>
